0cb1c483db6301b0001485d33b4bf2039283909a
Gustavo Xavier <gustavoxavier154@yahoo.com.br>
2019-10-15 12:38:25 -0300

    Alterado para Mãe Russia

--- Projeto Test/Program.cs
+++ Projeto Test/Program.cs
@@ -6,9 +6,42 @@ namespace Projeto_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Privet Brat");
 
-            Console.WriteLine("Poshili p'it vodke");
+            Console.WriteLine("Poshili pit' vodke moy drug?");
+
+            string resposta = Console.ReadLine();
+
+            while (resposta != "Davai davai brat" && resposta != "Njet Ya khotchu Koka Kola")
+            {
+                Console.WriteLine("Apenas aceitas respostas: <Davai davai brat> ou <Njet Ya khotchu Koka Kola> ");
+                resposta = Console.ReadLine();
+            }
+            if (resposta == "Davai davai brat")
+                Console.WriteLine("Россия – священная наша держава," +
+                                  "Россия – любимая наша страна." +
+                                  "Могучая воля, великая слава –" +
+                                  "Твоё достоянье на все времена!" +
+
+                                   "Припев:" +
+                                   "Славься, Отечество наше свободное," +
+                                   "Братских народов союз вековой," +
+                                   "Предками данная мудрость народная!" +
+                                   "Славься, страна! Мы гордимся тобой!" +
+
+                                   "От южных морей до полярного края" +
+                                   "Раскинулись наши леса и поля." +
+                                   "Одна ты на свете! Одна ты такая –" +
+                                   "Хранимая Богом родная земля!" +
+
+                                   "Припев" +
+
+                                   "Широкий простор для мечты и для жизни." +
+                                   "Грядущие нам открывают года." +
+                                   "Нам силу даёт наша верность Отчизне." +
+                                   "Так было, так есть и так будет всегда!");
+            else
+                Console.WriteLine("IDI NA HUI SUKA AMERICANSKI YEBAL");
 
             Console.ReadLine();
         }
